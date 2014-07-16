1ff096b08734f33788e66202593d82b16ce13629
David Ernst <github@dsernst.com>
2014-07-16 04:55:00 -0400

    fix typo

--- server/fixtures.js
+++ server/fixtures.js
@@ -15,7 +15,7 @@ if (Posts.find().count() === 0) {
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
-    author: 'sacha.profile.name',
+    author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: now - 7 * 3600 * 1000
   });
